e8adbf96ec9e40491eb88b6b602f2a1d5248541a
Derik Banning <deriklee451@gmail.com>
2022-12-08 14:07:54 -0700

    initial commit

---
+++ src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = 'https://bcw-sandbox.herokuapp.com/'
+export const useSockets = false
+export const domain = 'deriklee.us.auth0.com'
+export const clientId = 'q2RA1jWwMAmlQfTzH3RWOLB6SsVmPV8r'
+export const audience = 'https://classroom.com'
